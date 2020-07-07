1bc2221e7df06ae65eef44d940bf4bbbdd913722
Fonsy <ciabadiale@gmail.com>
2020-07-07 22:01:13 +0200

    No swallow

--- dwm/config.h
+++ dwm/config.h
@@ -32,13 +32,14 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class     instance  title           tags mask  iscentered   isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",    NULL,     NULL,           0,         1,           1,          0,          0,         -1 },
-	{ "Firefox", NULL,     NULL,           1 << 8,    0,		   0,          0,         -1,         -1 },
-	{ "discord", NULL,	   NULL,		   2,	      0,		   0,          0,          1,         -1 },
-	{ "st",      NULL,     NULL,           0,         0,		   0,          1,         -1,         -1 },
-	{ NULL,	 	 NULL,	   "pulsemixer",   0,	      1,		   1,          0,          1,         -1 },
-	{ NULL,      NULL,     "Event Tester", 0,         0,		   1,          0,          1,         -1 },
+	/* class     instance  title           		tags mask  iscentered   isfloating  isterminal  noswallow  monitor */
+	{ "Gimp",    NULL,     NULL,           		0,        1,           1,          0,          0,         -1 },
+	{ "Firefox", NULL,     NULL,           		1 << 8,   0,		   0,          0,         -1,         -1 },
+	{ "discord", NULL,	   NULL,		   		2,	      0,		   0,          0,          1,         -1 },
+	{ "st",      NULL,     NULL,           		0,        0,		   0,          1,         -1,         -1 },
+	{ NULL,	 	 NULL,	   "pulsemixer",   		0,	      1,		   1,          0,          1,         -1 },
+	{ NULL,      NULL,     "Event Tester", 		0,        0,		   1,          0,          1,         -1 },
+	{ NULL,      NULL,     "Payload Injector",  0,        0,		   0,          0,          1,         -1 }
 };
 
 /* layout(s) */
